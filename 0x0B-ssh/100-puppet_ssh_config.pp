be0735a1d314e42e9ad6b70df4a8b324cafb7346
Collins <collinsabeiku07@gmail.com>
2025-01-15 09:52:53 -0800

    updated Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ file {'/etc/ssh/ssh_config':
 }
 
 file_line {'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config/',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => 'PasswordAuthentication yes',
   replace => 'true',
